8058c7b5a792292ac63bd040c29bf6575a11dc88
Rully Afrizal <74956021+rullyafrizal@users.noreply.github.com>
2021-03-14 22:49:04 +0700

    ...

--- public/3.js
+++ public/3.js
@@ -266,7 +266,7 @@ var render = function() {
             "v-flex",
             {
               key: "campaign-" + campaign.id,
-              attrs: { xs12: "", sm8: "", md6: "", lg4: "" }
+              attrs: { xs11: "", sm8: "", md6: "", lg4: "" }
             },
             [_c("campaign-item", { attrs: { campaign: campaign } })],
             1

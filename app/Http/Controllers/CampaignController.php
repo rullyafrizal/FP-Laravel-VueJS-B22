b71d720d1f86ce782d8e3bd916d90b37d1f7ef70
Rully Afrizal <74956021+rullyafrizal@users.noreply.github.com>
2021-03-15 13:29:34 +0700

    ...

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -100,7 +100,7 @@ class CampaignController extends Controller
     }
 
     public function search($keyword){
-        $campaigns = Campaign::select('*')->whereRaw('(title)', 'ilike', '%'.$keyword.'%')->get();
+        $campaigns = Campaign::select('*')->where('title', 'ilike', '%'.$keyword.'%')->get();
 
         $data['campaigns'] = $campaigns;
 
